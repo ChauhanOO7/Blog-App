a6aaac61eac17ffcd6f6ead5d7c6a96337dd30f2
ChauhanOO7 <saurabhc7388@gmail.com>
2024-02-23 20:41:16 +0530

    eight commit

--- routes/staticroute.js
+++ routes/staticroute.js
@@ -7,18 +7,7 @@ const {comments}=require("../models/comments");
 const router=express.Router();
 const multer  = require('multer');
 const path=require("path");
-// const uploadpath=path.join(__dirname,"../tmp");
 let blogwanted;
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, uploadpath)
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null,`${Date.now()}-${file.originalname}`)
-//     }
-//   });
-
-// const upload=multer({storage:storage});
 
 
 router.get("/",async (req,res)=>{
