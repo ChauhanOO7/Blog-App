2a6c4cd65bda22d06766751e79757fa94d4c56bc
ChauhanOO7 <saurabhc7388@gmail.com>
2024-02-23 15:54:31 +0530

    third commit

---
+++ cloudinary.js
@@ -0,0 +1,9 @@
+const cloud=require("cloudinary").v2;
+
+cloud.config({ 
+    cloud_name: process.env.cloud_name, 
+    api_key: process.env.api_key, 
+    api_secret: process.env.api_secret, 
+}); 
+
+module.exports=cloud;
